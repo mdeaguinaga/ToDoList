a90fed2fc05f5f84c1d0133b2cfc6506ca6e4a3a
Manuel <isra_de@outlook.com>
2018-12-21 11:24:22 -0600

    Initial Commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Manuel De Aguinaga on 12/21/18.
+//  Copyright © 2018 Manuel De Aguinaga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
